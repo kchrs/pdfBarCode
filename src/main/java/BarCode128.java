f566f3246b511b642ec8763b45a252d01ea752d1
cchristopoulos <cchristopoulos@icap.gr>
2018-12-05 17:14:46 +0200

    latest

--- src/main/java/BarCode128.java
+++ src/main/java/BarCode128.java
@@ -1,35 +1,56 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.*;
-
+import org.apache.commons.lang3.StringUtils;
 
 
 public class BarCode128 {
 
     public static void main(String[] args) throws IOException, DocumentException {
 
-        Document document = new Document(new Rectangle(PageSize.A8).rotate());
-
-        //PdfPage pdfPage = new PdfPage();
-
+        Document document = new Document(new Rectangle(PageSize.B8).rotate());
 
         BaseFont fonty = BaseFont.createFont("/fonts/CaviarDreams.TTF", BaseFont.IDENTITY_H,       BaseFont.NOT_EMBEDDED);
         fonty.setSubset(true);
-        Phrase title = new Phrase("3D Bearded Cat Silicone Soft Cell Phone Cover For IPhone 6s / 6 - Pink", new Font(fonty,4));
+
+
+
+        String titleDesc="3D Bearded Cat Silicone Soft Cell Phone\n" +
+                         " Cover For IPhone 6s / 6 - Pink";
+
+        String priceString= "€ "+String.valueOf(4.90);
+       String splited[] = StringUtils.split(titleDesc);
+       List<String> tempSplittedlist = new ArrayList<>(Arrays.asList(splited));
+       tempSplittedlist.size();
+       // tempSplittedlist.length()
+
+        Phrase title = new Phrase(titleDesc, new Font(fonty,3));
+        Phrase price = new Phrase(priceString, new Font(fonty,3));
+
+        Paragraph titlePar = new Paragraph(title);
+        titlePar.setAlignment(Element.ALIGN_CENTER);
+
+        Paragraph pricePar = new Paragraph(price);
+        pricePar.setAlignment(Element.ALIGN_CENTER);
 
         PdfWriter writer = PdfWriter.getInstance(document,
-                new FileOutputStream("/Users/kchris/Desktop/test.pdf"));
+                new FileOutputStream("/pdf/test.pdf"));
 
         document.open();
-
-        document.add(new Paragraph(title));
+        document.add(titlePar);
+        document.add(pricePar);
 
         Barcode128 code128 = new Barcode128();
+
         code128.setGenerateChecksum(true);
         code128.setCode("500658");
+       // code128.setTextAlignment(Element.ALIGN_CENTER);
 
         document.add(code128.createImageWithBarcode(writer.getDirectContent(), null, null));
         document.close();
---
+++ src/main/java/Barcodes.java
@@ -0,0 +1,47 @@
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.pdf.Barcode;
+import com.itextpdf.text.pdf.BarcodeEAN;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Barcodes {
+    public static final String DEST = "/pdf/barcode_table.pdf";
+
+    public static void main(String[] args) throws IOException,
+            DocumentException {
+        File file = new File(DEST);
+        file.getParentFile().mkdirs();
+        new Barcodes().createPdf(DEST);
+    }
+
+    public void createPdf(String dest) throws IOException, DocumentException {
+        Document document = new Document();
+        PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(dest));
+        document.open();
+        PdfPTable table = new PdfPTable(1);
+        table.setWidthPercentage(20);
+        table.addCell(createBarcode(writer, String.format("%08d", 1)));
+
+//            for (int i = 0; i < 12; i++) {
+//            table.addCell(createBarcode(writer, String.format("%08d", i)));
+//        }
+        document.add(table);
+        document.close();
+    }
+
+    public static PdfPCell createBarcode(PdfWriter writer, String code) throws DocumentException, IOException {
+        BarcodeEAN barcode = new BarcodeEAN();
+        barcode.setCodeType(Barcode.EAN8);
+        barcode.setCode(code);
+        PdfPCell cell = new PdfPCell(barcode.createImageWithBarcode(writer.getDirectContent(), BaseColor.BLACK, BaseColor.GRAY), true);
+        cell.setPadding(10);
+        return cell;
+    }
+}
